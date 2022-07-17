78e74199b75f507b6ce15bd66f2b8cd695d9a78d
DENKLI <denis.klishevich@gmail.com>
2022-07-17 22:13:52 +0300

    Выполнил ДЗ-3

---
+++ src/lesson03/Conditions.java
@@ -0,0 +1,128 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class Conditions { // ДЗ, часть №1
+
+    public static void main(String[] args) {
+
+        // Задача 1. Написать программу для вывода времени года по номеру месяца. Должно быть 2 варианта
+        // реализации: switch-case и if-else-if
+        int numberMonth = 8;
+        if (numberMonth == 1 || numberMonth == 2 || numberMonth == 12) {
+            System.out.println("Winter");
+        } else if (numberMonth >= 3 && numberMonth < 6) {
+            System.out.println("Spring");
+        } else if (numberMonth >= 6 && numberMonth <= 8) {
+            System.out.println("Summer");
+        } else if (numberMonth > 8 && numberMonth < 12) {
+            System.out.println("Autumn");
+        }
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите номер месяца: ");
+        int numberMonth2 = sc.nextInt();
+        switch (numberMonth2) {
+            case 1, 2, 12:
+                System.out.println("Данный месяц соответствует поре года - " + "Зима");
+                break;
+            case 3, 4, 5:
+                System.out.println("Данный месяц соответствует поре года - " + "Весна");
+                break;
+            case 6, 7, 8:
+                System.out.println("Данный месяц соответствует поре года - " + "Лето");
+                break;
+            case 9, 10, 11:
+                System.out.println("Данный месяц соответствует поре года - " + "Осень");
+                break;
+            default:
+                System.out.println("Месяца с таким номером не существует");
+        }
+        System.out.println();
+
+        System.out.print("Введите номер месяца: ");
+        int numberMonth3 = sc.nextInt();
+        switch (numberMonth3) {  // Попробовал добавить на вывод еще и название месяца
+            case 1:
+                System.out.println("Данный месяц - Январь, пора года - Зима");
+                break;
+            case 2:
+                System.out.println("Данный месяц - Февраль, пора года - Зима");
+                break;
+            case 3:
+                System.out.println("Данный месяц - Март, пора года - Весна");
+                break;
+            case 4:
+                System.out.println("Данный месяц - Апрель, пора года - Весна");
+                break;
+            case 5:
+                System.out.println("Данный месяц - Май, пора года - Весна");
+                break;
+            case 6:
+                System.out.println("Данный месяц - Июнь, пора года - Лето");
+                break;
+            case 7:
+                System.out.println("Данный месяц - Июль, пора года - Лето");
+                break;
+            case 8:
+                System.out.println("Данный месяц - Август, пора года - Лето");
+                break;
+            case 9:
+                System.out.println("Данный месяц - Сентябрь, пора года - Осень");
+                break;
+            case 10:
+                System.out.println("Данный месяц - Октябрь, пора года - Осень");
+                break;
+            case 11:
+                System.out.println("Данный месяц - Ноябрь, пора года - Осень");
+                break;
+            case 12:
+                System.out.println("Данный месяц - Декабрь, пора года - Зима");
+                break;
+            default:
+                System.out.println("Месяца с таким номером не существует");
+        }
+        System.out.println();
+
+        // Задача 2. Напишите программу, которая будет принимать на вход число (можно задать явно в коде) и на выход
+        // будет выводить сообщение о четности числа.
+        System.out.print("Введите число: ");
+        int number = sc.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Данное число является четным");
+        } else {
+            System.out.println("Данное число является нечетным");
+        }
+        System.out.println();
+
+        // Задача 3. Для числа t (температура на улице) вывести:
+        // - Если t > 15, то вывести «Тепло»
+        // - Если t <= 15 и t > -5, то вывести «Нормально»
+        // - Если t <= 5, то вывести «Холодно»
+        int t = 19;
+        if (t > 15) {
+            System.out.println("Тепло");
+        } else if (t <= 15 && t > -5) {
+            System.out.println("Нормально");
+        } else if (t <= -5) {
+            System.out.println("Холодно");
+        }
+        System.out.println();
+
+        // Задача 4. По введенному (можно задать явно) номеру определить цвет радуги (1 – красный, 4 – зеленый и т.д.)
+        int numberColor = 5;
+        switch (numberColor) {
+            case 1 -> System.out.println("Красный");   // Программа подсказала упрощенный вариант записи кода
+            case 2 -> System.out.println("Оранжевый");
+            case 3 -> System.out.println("Желтый");
+            case 4 -> System.out.println("Зеленый");
+            case 5 -> System.out.println("Голубой");
+            case 6 -> System.out.println("Синий");
+            case 7 -> System.out.println("Фиолетовый");
+            default -> System.out.println("У радуги только семь явных цветов");
+        }
+        System.out.println();
+
+    }
+}
---
+++ src/lesson03/Cycles.java
@@ -0,0 +1,64 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class Cycles { // ДЗ, часть №2
+
+    public static void main(String[] args) {
+
+        // Задача 1. При помощи цикла for вывести на экран нечетные числа от 1 до 99. (использовать инкремент)
+        for (int i = 1; i < 100; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("\n");
+
+        // Задача 2. При помощи цикла for вывести на экран числа от 15 до 1. (использовать декремент)
+        for (int i = 15; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        // Задача 3. Напишите программу, где пользователь вводит любое целое положительное число - N. Программа
+        // суммирует все числа от 1 до N. Для ввода числа используйте класс Scanner.
+        System.out.print("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int result = 0;
+        for (int i = 1; i <= number; i++) {
+            result += i;
+            if (i == number) {
+                System.out.println("Сумма чисел от 1 до " + number + " = " + result);
+            }
+        }
+        System.out.println();
+
+        // Задача 4. Напишите программу, которая будет выводить на экран такую последовательность: 7 14 21 28 35 42
+        // 49 56 63 70 77 84 91 98. В решении использовать цикл while
+        int count = 6;
+        while (count <= 98) {
+            if (++count % 7 == 0) {
+                System.out.print(count + " ");
+            }
+        }
+        System.out.println("\n");
+
+        // Задача 5. Вывести 10 первых чисел последовательности 0, -5, -10, -15...
+        for (int i = 0; i >= -45; i--) {
+            if (i % 5 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("\n");
+
+        // Задача 6. Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно
+        int numberSquare = 0;
+        for (int i = 10; i <= 20; i++) {
+            numberSquare = i * i;
+            System.out.print(numberSquare + " ");
+        }
+        System.out.println();
+
+    }
+}
